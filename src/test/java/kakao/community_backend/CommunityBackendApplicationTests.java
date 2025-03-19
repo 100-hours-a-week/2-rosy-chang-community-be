27a34fd8e8550f70774560550ea5ca6aaec10d19
beviosy <cjstk0021@gmail.com>
2025-03-19 15:46:16 +0900

    made project structure

---
+++ src/test/java/kakao/community_backend/CommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kakao.community_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
