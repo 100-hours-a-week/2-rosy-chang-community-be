8a462f2639ae41713fe1ee0ce4a8e70bfbb91c65
beviosy <cjstk0021@gmail.com>
2025-03-23 21:25:39 +0900

    feat: connected with FE

--- src/main/java/kakao/community_backend/config/WebMvcConfig.java
+++ src/main/java/kakao/community_backend/config/WebMvcConfig.java
@@ -24,7 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://127.0.0.1:5500") // Live Server의 기본 포트
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
